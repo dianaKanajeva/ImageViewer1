fd447dcee1a7ba21aa68b1dc828a55df3fefe630
dianaKanajeva <dkmurze@gmail.com>
2016-05-01 21:28:13 +0300

    Added files via upload

---
+++ PictureViewer1Tests/PictureViewer1Tests.m
@@ -0,0 +1,40 @@
+//
+//  PictureViewer1Tests.m
+//  PictureViewer1Tests
+//
+//  Created by Diana Kanajeva on 01/05/16.
+//  Copyright (c) 2016 Diana Kanajeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PictureViewer1Tests : XCTestCase
+
+@end
+
+@implementation PictureViewer1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
